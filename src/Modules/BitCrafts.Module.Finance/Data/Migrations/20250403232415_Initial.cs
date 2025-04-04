1187f7ac12c644e0cdd6d66b92aea3cf53a3929f
BenzSoftware <204579751+benzsoftware@users.noreply.github.com>
2025-04-04 00:44:43 +0000

    Add initial migrations and Avalonia test project setup
    This commit introduces initial database migrations for the UserAccounts and Finance modules, setting up core entities and relationships. Additionally, it includes the project configuration for Avalonia test cases with dependencies and references.

---
+++ src/Modules/BitCrafts.Module.Finance/Data/Migrations/20250403232415_Initial.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BitCrafts.Module.Finance.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: true),
+                    LastName = table.Column<string>(type: "text", nullable: true),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    Password = table.Column<string>(type: "text", nullable: true),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BankAccounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AccountName = table.Column<string>(type: "text", nullable: true),
+                    AccountNumber = table.Column<string>(type: "text", nullable: true),
+                    Balance = table.Column<decimal>(type: "numeric", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankAccounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BankAccounts_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BankTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TransactionDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    BankAccountId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankTransactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BankTransactions_BankAccounts_BankAccountId",
+                        column: x => x.BankAccountId,
+                        principalTable: "BankAccounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BankTransfers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SourceAccountId = table.Column<int>(type: "integer", nullable: false),
+                    DestinationAccountId = table.Column<int>(type: "integer", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    TransferDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    SourceTransactionId = table.Column<int>(type: "integer", nullable: true),
+                    DestinationTransactionId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankTransfers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BankTransfers_BankAccounts_DestinationAccountId",
+                        column: x => x.DestinationAccountId,
+                        principalTable: "BankAccounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BankTransfers_BankAccounts_SourceAccountId",
+                        column: x => x.SourceAccountId,
+                        principalTable: "BankAccounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BankTransfers_BankTransactions_DestinationTransactionId",
+                        column: x => x.DestinationTransactionId,
+                        principalTable: "BankTransactions",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_BankTransfers_BankTransactions_SourceTransactionId",
+                        column: x => x.SourceTransactionId,
+                        principalTable: "BankTransactions",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankAccounts_UserId",
+                table: "BankAccounts",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankTransactions_BankAccountId",
+                table: "BankTransactions",
+                column: "BankAccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankTransfers_DestinationAccountId",
+                table: "BankTransfers",
+                column: "DestinationAccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankTransfers_DestinationTransactionId",
+                table: "BankTransfers",
+                column: "DestinationTransactionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankTransfers_SourceAccountId",
+                table: "BankTransfers",
+                column: "SourceAccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankTransfers_SourceTransactionId",
+                table: "BankTransfers",
+                column: "SourceTransactionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BankTransfers");
+
+            migrationBuilder.DropTable(
+                name: "BankTransactions");
+
+            migrationBuilder.DropTable(
+                name: "BankAccounts");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
